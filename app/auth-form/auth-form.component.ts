ab4d8b18f2a2123f3236a1eda5b8843938b15559
Ouimet, Frederic <frederic.ouimet@sap.com>
2018-09-17 09:04:06 -0400

    08-viewchild-template-refs

--- app/auth-form/auth-form.component.ts
+++ app/auth-form/auth-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef, Output, ViewChildren, AfterViewInit, EventEmitter, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
+import { Component, ChangeDetectorRef, ElementRef, Output, ViewChild, ViewChildren, AfterViewInit, EventEmitter, ContentChildren, QueryList, AfterContentInit } from '@angular/core';
 
 import { AuthRememberComponent } from './auth-remember.component';
 import { AuthMessageComponent } from './auth-message.component';
@@ -13,19 +13,13 @@ import { User } from './auth-form.interface';
         <ng-content select="h3"></ng-content>
         <label>
           Email address
-          <input type="email" name="email" ngModel>
+          <input type="email" name="email" ngModel #email>
         </label>
         <label>
           Password
           <input type="password" name="password" ngModel>
         </label>
         <ng-content select="auth-remember"></ng-content>
-        <auth-message 
-          [style.display]="(showMessage ? 'inherit' : 'none')">
-        </auth-message>
-        <auth-message 
-          [style.display]="(showMessage ? 'inherit' : 'none')">
-        </auth-message>
         <auth-message 
           [style.display]="(showMessage ? 'inherit' : 'none')">
         </auth-message>
@@ -38,6 +32,8 @@ export class AuthFormComponent implements AfterContentInit, AfterViewInit {
 
   showMessage: boolean;
 
+  @ViewChild('email') email: ElementRef;
+
   @ViewChildren(AuthMessageComponent) message: QueryList<AuthMessageComponent>;
 
   @ContentChildren(AuthRememberComponent) remember: QueryList<AuthRememberComponent>;
@@ -47,6 +43,7 @@ export class AuthFormComponent implements AfterContentInit, AfterViewInit {
   constructor(private cd: ChangeDetectorRef) {}
 
   ngAfterViewInit() {
+    console.log(this.email);
     if (this.message) {
       this.message.forEach((message) => {
         message.days = 30;
