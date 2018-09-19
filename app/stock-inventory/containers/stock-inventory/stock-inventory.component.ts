c08caa6d75b8c94b5ebfa78c87b83bc54f3027bf
Ouimet, Frederic <frederic.ouimet@sap.com>
2018-09-19 13:32:07 -0400

    31-componentizing-formgroups

--- app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -8,16 +8,17 @@ import { FormControl, FormGroup, FormArray } from '@angular/forms';
     <div class="stock-inventory">
       <form [formGroup]="form" (ngSubmit)="onSubmit()">
 
-        <div formGroupName="store">
-          <input 
-            type="text" 
-            placeholder="Branch ID"
-            formControlName="branch">
-          <input 
-            type="text" 
-            placeholder="Manager Code"
-            formControlName="code">
-        </div>
+        <stock-branch
+          [parent]="form">
+        </stock-branch>
+
+        <stock-selector
+          [parent]="form">
+        </stock-selector>
+
+        <stock-products
+          [parent]="form">
+        </stock-products>
 
         <div class="stock-inventory__buttons">
           <button 
@@ -36,9 +37,14 @@ import { FormControl, FormGroup, FormArray } from '@angular/forms';
 export class StockInventoryComponent {
   form = new FormGroup({
     store: new FormGroup({
-      branch: new FormControl('B182'),
-      code: new FormControl('1234')
-    })
+      branch: new FormControl(''),
+      code: new FormControl('')
+    }),
+    selector: new FormGroup({
+      product_id: new FormControl(''),
+      quantity: new FormControl(10)
+    }),
+    stock: new FormArray([])
   })
 
   onSubmit() {
