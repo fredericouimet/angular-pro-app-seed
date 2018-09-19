c7fbc9006d5b823a9100e3505a25ba51351f537e
Ouimet, Frederic <frederic.ouimet@sap.com>
2018-09-19 13:33:29 -0400

    34-formarray-add

--- app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -16,7 +16,8 @@ import { Product } from '../../models/product.interface';
 
         <stock-selector
           [parent]="form"
-          [products]="products">
+          [products]="products"
+          (added)="addStock($event)">
         </stock-selector>
 
         <stock-products
@@ -64,6 +65,11 @@ export class StockInventoryComponent {
       product_id: new FormControl(parseInt(stock.product_id, 10) || ''),
       quantity: new FormControl(stock.quantity || 10)
     });
+  }
+
+  addStock(stock) {
+    const control = this.form.get('stock') as FormArray;
+    control.push(this.createStock(stock));
   }
 
   onSubmit() {
